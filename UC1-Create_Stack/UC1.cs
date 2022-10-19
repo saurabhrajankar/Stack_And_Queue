251ea9314cf3d0787b6f8180830457b7da6e915c
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-19 12:18:43 +0530

    create a Stack of 56->30->70

---
+++ UC1-Create_Stack/UC1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_Create_Stack
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class Stack
+    {
+        public Node top = null;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(node.data + ":Inserted SuccessFully");
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("Values In Stack Are:");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+    internal class SQ1
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Add(70);
+            s.Add(30);
+            s.Add(56);
+            s.Display();
+        }
+    }
+}
